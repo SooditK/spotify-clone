928493676eab7467068188363bbafb5a7bead061
SooditK <sooditkumarabc@gmail.com>
2021-12-14 19:44:19 +0530

    🍻 ♻️ Serverless Login w/ Cookies Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return <div>Home</div>;
+const Home = () => {
+  return <div>Home</div>
 }
+
+export default Home;
