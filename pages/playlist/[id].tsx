ca8359a1ffede09a4b32503037d3fa2c49ee830c
SooditK <sooditkumarabc@gmail.com>
2021-12-29 23:10:50 +0530

    🍻 💥 🚑 Added playerBar, Updated Schema

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,16 +1,58 @@
-import prisma from '../../lib/prisma'
+import GradientLayout from '../../components/gradientLayout'
+import SongTable from '../../components/songsTable'
 import { validateToken } from '../../lib/auth'
+import prisma from '../../lib/prisma'
 
-const Playlist = ({playlist}) => {
-  return <div>{playlist.name}</div>
+const getBGColor = (id) => {
+  const colors = [
+    'red',
+    'green',
+    'blue',
+    'orange',
+    'purple',
+    'gray',
+    'teal',
+    'yellow',
+  ]
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)]
 }
 
-export const getServerSideProps = ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN)
-  const [playlist] = prisma.playlist.findMany({
+const Playlist = ({ playlist }) => {
+  const color = getBGColor(playlist.id)
+
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <SongTable songs={playlist.songs} />
+    </GradientLayout>
+  )
+}
+
+export const getServerSideProps = async ({ query, req }) => {
+  let user
+
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN)
+  } catch (e) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/signin',
+      },
+    }
+  }
+
+  const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
@@ -25,11 +67,9 @@ export const getServerSideProps = ({ query, req }) => {
       },
     },
   })
+
   return {
-    props: {
-      playlist,
-    },
+    props: { playlist },
   }
 }
-
 export default Playlist
